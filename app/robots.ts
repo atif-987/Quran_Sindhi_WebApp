829e494f3596e94dccb28d8251ed6f17b4086d51
Muhammad Atif <atif.mtpixels@gmail.com>
2025-11-13 12:50:43 +0500

    Simplify robots.txt to fix Google crawling

--- app/robots.ts
+++ app/robots.ts
@@ -2,21 +2,11 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: '/api/',
-      },
-      {
-        userAgent: 'Googlebot',
-        allow: '/',
-      },
-      {
-        userAgent: 'Bingbot',
-        allow: '/',
-      },
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/api/',
+    },
     sitemap: 'https://quransindhitarjumo.vercel.app/sitemap.xml',
   };
 }
